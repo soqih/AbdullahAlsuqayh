602165bff8ece41bd8aa8dd8978b974f528c97a8
عبدالله ابراهيم <abdullahibrahim@bdallhs-MacBook-Air.local>
2023-02-24 21:03:19 +0300

    enhance 404 page

--- src/pages/404/NotFound.js
+++ src/pages/404/NotFound.js
@@ -1,18 +1,18 @@
-import { Link } from "react-router-dom";
 import { motion } from "framer-motion"
 
 const NotFound = () => {
-    
+
     return (
         <motion.div
             // Prop that will animate when component is removed from DOM
             exit={{ opacity: 0 }}
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}>
-            <div className="not-found">
-                <h2 style={{marginBottom:'0.4rem'}}>Sorry</h2>
-                <p style={{marginBottom:'1rem'}}>That page cannot be found</p>
-                <Link  style={{color:'#F2786D', borderBottom:'1px solid #F2786D', padding:'0.3rem 0rem'}} to="/home">  Back to the homepage </Link>
+            <div className="not-found" >
+                    <h1 style={{ marginBottom: '1rem' , fontSize:'4rem'}}>404 😪</h1>
+                    <p style={{ marginBottom: '1rem' }}> <b>Sorry</b>  that page cannot be found</p>
+                
+
             </div>
         </motion.div>
 
